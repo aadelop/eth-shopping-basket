77a3d279c397d1efd8f14223ba6e2dfd9ab73dd4
Alfredo Delgado L <aadelop@gmail.com>
2024-09-13 07:19:04 +0200

    Added logic and comunication with server using react-router and react query components

--- front/src/components/Products.jsx
+++ front/src/components/Products.jsx
@@ -1,3 +1,31 @@
+import {useQuery} from 'react-query'
+import {Link} from 'react-router-dom'
+
 export function Products() {
-    return <h2>Products</h2>
+    const {data,isLoading} = useQuery("products", ()=>{
+        return fetch("http://localhost:5555/products").then(res => res.json())
+    })
+
+    if(isLoading){
+        return <div><h2>Products</h2>
+            Cargando..</div>
+    }
+
+    return <div>
+            <h2>Products</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Nombre</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map((product,index) => {
+                        return <tr key={index}>
+                               <td > <Link to={`/product/${product.ProductID}`}>{product.ProductName}</Link></td>
+                        </tr>
+                    })}
+                </tbody>
+            </table>
+        </div>
 }
