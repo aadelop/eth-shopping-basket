482ae1d1cc6c7adf4c282aa6be7863f855c91264
Alfredo Delgado L <aadelop@gmail.com>
2024-09-13 07:19:40 +0200

    Added logic and comunication of single product call  with server using react-router and react query components

--- front/src/components/Product.jsx
+++ front/src/components/Product.jsx
@@ -1,3 +1,16 @@
+import {useQuery} from 'react-query'
+import {useParams} from 'react-router-dom'
+
 export function Product() {
-    return <h2>Info of the product</h2>
+    const params = useParams()
+    const {data, isLoading} = useQuery('product', () => {
+        return fetch(`http://localhost:5555/products/${params.id}`).then(res => res.json())
+    })
+
+    if(isLoading){
+        return <div><h2>Products</h2>
+            Cargando..</div>
+    }
+
+    return <p> {JSON.stringify(data)}</p>
 }
