61d4590eacc2aa9835668fa3446b865f04cf3df4
Alfredo Delgado L <aadelop@gmail.com>
2024-09-13 07:20:17 +0200

    Added react-query in package of the project

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import {QueryClient, QueryClientProvider} from 'react-query'
 import './index.css'
 import {Home} from './components/Home'
 import {Basket} from './components/Basket'
@@ -8,19 +9,25 @@ import {Products} from './components/Products'
 import {Product} from './components/Product'
 
 
+
+const queryClient = new QueryClient();
+
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home></Home>}>
-          <Route index element={<Products></Products>}></Route>
-          <Route path="*" element={<Products></Products>}></Route>
-          <Route path="products" element={<Products></Products>}></Route>
-          <Route path="product/:id" element={<Product></Product>}></Route>
-          <Route path="basket" element={<Basket></Basket>}></Route>
-      </Route>
-    </Routes>
-    </BrowserRouter>
-    
+    <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home></Home>}>
+              <Route index element={<Products></Products>}></Route>
+              <Route path="*" element={<Products></Products>}></Route>
+              <Route path="products" element={<Products></Products>}></Route>
+              <Route path="product/:id" element={<Product></Product>}></Route>
+              <Route path="basket" element={<Basket></Basket>}></Route>
+          </Route>
+        </Routes>
+        </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>,
 )
