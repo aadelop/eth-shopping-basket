93f843b222cb1365c161bff11bd4a0460a455403
Alfredo Delgado L <aadelop@gmail.com>
2024-09-17 08:28:34 +0200

    Added form to add the element to the basket-cart and maitain the state of the cart trought the components

--- front/src/components/Product.jsx
+++ front/src/components/Product.jsx
@@ -1,11 +1,32 @@
 import {useQuery} from 'react-query'
 import {useParams} from 'react-router-dom'
+import {useForm } from 'react-hook-form'
+import { useContext } from 'react'
+import {Context} from '../main.jsx'
 
 export function Product() {
     const params = useParams()
+    const [bstate, setBstate] = useContext(Context)
+    const quantity = bstate.basket.find(i => i.product.ProductID == params.id)?.quantity
+
+    const {register, handleSubmit} = useForm({
+        defaultValues:{quantity:quantity}
+    })
     const {data, isLoading} = useQuery('product', () => {
         return fetch(`http://localhost:5555/products/${params.id}`).then(res => res.json())
     })
+
+    function onSubmit(datos){
+        console.log(datos)
+        setBstate({
+            ...bstate, basket:[...bstate.basket.filter(i => i.product.ProductID != data.ProductID),
+            {
+                product:data,
+                quantity: datos.quantity,
+                total: datos.quantity * data.UnitPrice
+            }]
+        })
+    }
 
     if(isLoading){
         return <div><h2>Products</h2>
@@ -30,5 +51,15 @@ export function Product() {
                 </tr>
             </thead>
         </table>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="form-group">
+                <label htmlFor="">Enter the quantity</label>
+                <input {...register('quantity')} type="number" className='form-control' />
+            </div>
+            <button className="btn btn-primary mt-3">Add to cart</button>
+        </form>
+        <div>
+            {JSON.stringify(bstate)}
+        </div>
     </div>
 }
