aff23fe5db7e964d42dfe75d69046147b3ccc218
Alfredo Delgado L <aadelop@gmail.com>
2024-09-17 09:28:38 +0200

    Complete Basket component that works like a shopping cart with all the products and its total price, where we will pay the purchase

--- front/src/components/Basket.jsx
+++ front/src/components/Basket.jsx
@@ -1,3 +1,34 @@
+import { useContext } from "react";
+import {Context} from '../main'
+
 export function Basket(){
-    return <h2>Basket</h2>
+    const [bstate, setBstate] = useContext(Context)
+    const total = bstate.basket.reduce((acc, item) => acc + item.total, 0)
+
+    return <div>
+        <table className="table">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Price</th>
+                    <th>Quantity</th>
+                    <th>Total</th>
+
+                </tr>
+            </thead>
+            <tbody>
+                {bstate.basket.map(i => (
+                    <tr key={i.product.ProductID}>
+                        <td>{i.product.ProductID}</td>
+                        <td>{i.product.ProductName}</td>
+                        <td>{i.product.UnitPrice}</td>
+                        <td>{i.quantity}</td>
+                        <td>{i.total}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+        <h3>Total: {total}</h3>
+    </div>
 }
