d1d68855a1a1f7de27105a6d33dde27085ea9603
Alfredo Delgado L <aadelop@gmail.com>
2024-09-20 08:33:11 +0200

    Added ethers librarie and logic to send payment request with the eth node blockchain using metamaks

--- front/src/components/Basket.jsx
+++ front/src/components/Basket.jsx
@@ -1,10 +1,42 @@
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import {Context} from '../main'
+import {ethers} from 'ethers'
+import {Link} from 'react-router-dom'
 
 export function Basket(){
     const [bstate, setBstate] = useContext(Context)
     const total = bstate.basket.reduce((acc, item) => acc + item.total, 0)
+    const [account, setAccount] = useState(null)
+    const [txOk, setTxOk] = useState(null)
+    const [txKo, setTxKo] = useState(null)
+    useEffect(() =>{
+        window.ethereum && window.ethereum.request({
+            method: 'eth_requestAccounts'
+        }).then(accounts =>{
+            setAccount(accounts[0])
+            ethereum.on('accountsChanged', (accounts)=>{
+                setAccount(accounts[0])
+            })
+        })
+    }, [])
+    async function pay(){
+        const txParams ={
+            to: "0x7817443d0FD4013F771964Fd4837d88441Dc6962",
+            from: account,
+            value: ethers.toBeHex(ethers.parseEther(total.toString()))
+        }
 
+        try {
+            const tx = await ethereum.request({
+                method:"eth_sendTransaction" ,
+                params:[txParams]
+            })
+            setTxOk(tx)
+        } catch (error) {
+            setTxKo(error)
+        }
+        console.log(txParams);
+    }
     return <div>
         <table className="table">
             <thead>
@@ -21,7 +53,7 @@ export function Basket(){
                 {bstate.basket.map(i => (
                     <tr key={i.product.ProductID}>
                         <td>{i.product.ProductID}</td>
-                        <td>{i.product.ProductName}</td>
+                        <td><Link to={`/product/${i.product.ProductID}`}>{i.product.ProductName}</Link></td>
                         <td>{i.product.UnitPrice}</td>
                         <td>{i.quantity}</td>
                         <td>{i.total}</td>
@@ -30,5 +62,9 @@ export function Basket(){
             </tbody>
         </table>
         <h3>Total: {total}</h3>
+        <h3>{account}</h3>
+        <button onClick={() => pay() } className="btn btn-primary">Pay</button>
+        {txOk && <p> {txOk} </p>}
+        {txKo && <p> {txKo} </p>}
     </div>
 }
